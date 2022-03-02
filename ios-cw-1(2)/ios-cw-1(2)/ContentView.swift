c9ca8f339d512e52b34d4b77de24630d44f576e1
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-02 19:53:58 +0300

    Done
    .

---
+++ ios-cw-1(2)/ios-cw-1(2)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-cw-1(2)
+//
+//  Created by Mohammed Alqattan on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, my name is Mohammed!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-cw-1(2)/ios-cw-1(2)/ios_cw_1_2_App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw_1_2_App.swift
+//  ios-cw-1(2)
+//
+//  Created by Mohammed Alqattan on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw_1_2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
